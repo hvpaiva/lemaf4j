a1d514d9f72248a67f526cdbbaf783c3f0e3d212
Highlander Vilela Paiva Silva <highlanderpaiva.lemaf@gmail.com>
2018-12-26 01:23:06 -0200

    Interface que provem uma representação do tipo como string

---
+++ lemaf4j-core/src/main/java/br/ufla/lemaf/ti/lemaf4j/vo/AsStringCapable.java
@@ -0,0 +1,19 @@
+package br.ufla.lemaf.ti.lemaf4j.vo;
+
+/**
+ * Provem uma representação em String
+ * de si mesmo.
+ *
+ * @author Highlander Paiva
+ * @since 1.0
+ */
+public interface AsStringCapable {
+
+    /**
+     * Retorna este tipo como String.
+     *
+     * @return A string
+     */
+    String asString();
+
+}

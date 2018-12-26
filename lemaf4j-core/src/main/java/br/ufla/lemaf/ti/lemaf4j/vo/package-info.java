c1e6f38294c1f31507c161a2abc1f4f9b19e75ad
Highlander Vilela Paiva Silva <highlanderpaiva.lemaf@gmail.com>
2018-12-26 01:21:37 -0200

    Criando package de VO

---
+++ lemaf4j-core/src/main/java/br/ufla/lemaf/ti/lemaf4j/vo/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contém objetos imutáveis que representam Value Objects, ou seja,
+ * objetos cuja sua igualdade não são baseadas em suas igualdades.
+ * Isto significa que as instâncias deste tipo são iguais quando seus valores
+ * são o mesmo, não necessariamente sendo os mesmos objetos.
+ */
+package br.ufla.lemaf.ti.lemaf4j.vo;

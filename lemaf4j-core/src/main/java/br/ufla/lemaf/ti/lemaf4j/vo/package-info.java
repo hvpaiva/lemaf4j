8b1095bcefdaaebb0625faaa9da3affe07c47486
Highlander Vilela Paiva Silva <highlanderpaiva.lemaf@gmail.com>
2018-12-26 12:12:31 -0200

    Adicionando ao package info os conversores e a regra de @Votoed

--- lemaf4j-core/src/main/java/br/ufla/lemaf/ti/lemaf4j/vo/package-info.java
+++ lemaf4j-core/src/main/java/br/ufla/lemaf/ti/lemaf4j/vo/package-info.java
@@ -4,4 +4,15 @@
  * Isto significa que as instâncias deste tipo são iguais quando seus valores
  * são o mesmo, não necessariamente sendo os mesmos objetos.
  */
+@Vetoed
+@XmlJavaTypeAdapters({
+        @XmlJavaTypeAdapter(value = UUIDConverter.class, type = UUID.class),
+        @XmlJavaTypeAdapter(value = LocaleConverter.class, type = Locale.class)
+})
 package br.ufla.lemaf.ti.lemaf4j.vo;
+
+import javax.enterprise.inject.Vetoed;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
+import java.util.Locale;
+import java.util.UUID;
